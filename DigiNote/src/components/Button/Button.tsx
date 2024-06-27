9bf12aee1f963809af4eced9f5ccbb1686c5cd3f
ImanolCB <imacallebara@gmail.com>
2024-06-27 19:12:54 +0200

    Modifyed navBar with responsive functionality

--- DigiNote/src/components/Button/Button.tsx
+++ DigiNote/src/components/Button/Button.tsx
@@ -1,19 +1,21 @@
-// import React from 'react';
-
 interface IButton {
   type: 'button' | 'submit' | 'reset';
   name?: string;
   value?: any;
-  content?: string;
+  content?: string,
+  state: boolean,
+  onClick?: () => void; // Propiedad opcional para manejar el evento click
 }
 
-export default function Button({ type, name, value, content='Button' }: IButton) {
+export default function Button(
+    { type, name, value, content='Button', state }: IButton){
+
   return (
     <button
       type={type}
       name={name}
       value={value}
-      className="text-white bg-sky-500 hover:bg-white/30 hover:text-gray-800 px-4 py-2 border border-sky-500 rounded-lg transition duration-500"
+      className={ `text-white bg-sky-500 hover:bg-white/30 hover:text-gray-800  border border-sky-500 rounded-lg transition duration-500 ${state ? 'px-4 py-2' : 'block w-full text-left'}` }
     >
       {content}
     </button>
--- DigiNote/src/components/NavBar/NavBar.tsx
+++ DigiNote/src/components/NavBar/NavBar.tsx
@@ -1,25 +1,49 @@
-import Button from "../Button/Button"
+import { useState } from 'react';
+import Button from '../Button/Button';
+import "./../../assets/navBar.css";
 
-const logo = './../../../public/logo.svg'
+const logo = './../../../public/logo.svg';
 
 export default function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <>
-      <nav className="bg-sky-400/60 backdrop-blur-md shadow-sky-200 shadow-md fixed w-full top-0 left-0 z-10">
-        <div className="mx-2 px-4 py-2 flex justify-between items-center">
-          {/* Left Side - Logo and About */}
-          <div className="flex items-center space-x-4">
-            <img src={logo} alt="Company Logo" className="h-20 w-20" />
-            <span className="text-white text-lg font-bold">DigiNote</span>
+    <nav className="bg-sky-400/60 backdrop-blur-md shadow-sky-200 shadow-md fixed w-full min-w-72 top-0 left-0 z-10">
+      <div className=" mx-auto px-4 py-2 flex justify-between items-center relative">
+        {/* Left Side - Logo and About */}
+        <div className="flex items-center space-x-4">
+          <img src={logo} alt="Company Logo" className="h-12 w-12 md:h-20 md:w-20" />
+          <span className="text-white text-lg font-bold">DigiNote</span>
+          <div className="hidden md:flex space-x-4">
             <a href="#about" className="text-white hover:text-gray-600">About</a>
           </div>
-          {/* Right Side - Mi cuenta */}
-          <div>
-            {/* <button className="text-white bg-sky-500 hover:bg-white/30 hover:text-gray-800 px-4 py-2  border border-sky-500 rounded-lg transition duration-500">Mi cuenta</button> */}
-            <Button type="button" content="Mi cuenta" />
-          </div>
         </div>
-      </nav>
-    </>
-  )
+        {/* Right Side - Mi cuenta */}
+        <div className="hidden md:flex">
+          <Button type="button" content="Mi cuenta" state={isOpen} onClick={toggleMenu} />
+        </div>
+        {/* Mobile Menu Button displayer */}
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="text-white focus:outline-none">
+            <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"} />
+            </svg>
+          </button>
+        </div>
+        {/* Mobile Menu Content */}
+        {isOpen && (
+          <div className="menu-content absolute top-full left-0 right-0 bg-white shadow-md mt-2 rounded-md overflow-hidden md:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+              <a href="#about" className="block text-gray-800 hover:text-sky-400 hover:bg-sky-400/10 py-4 px-2 rounded-md">About</a>
+              <Button type="button" content="Mi cuenta" state={isOpen} onClick={toggleMenu} />
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 }
